56c45d5f02d6287f76c453bd82874781357cdd34
revinmuhammadwiliyamzah-hash <revinmuhammadwiliyamzah@gmail.com>
2025-10-21 21:14:34 +0700

    Add files via upload

---
+++ latihan js/script.js
@@ -0,0 +1,10 @@
+
+
+// mencari nilai ganjil/genap
+let nilai = prompt('Masukan nilai:')
+let hasil = nilai % 2;
+if(hasil = 1) {
+    alert("bilangan" + nilai + "adalah bilangan ganjil")
+} else {
+    alert("bilangan" + nilai + "adalah bilangan genap")
+}
